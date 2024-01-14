be84d689429fbcef7b8eaff9e07de7c5d89511a0
yari-dewalt <yari.dewalt@gmail.com>
2024-01-14 13:56:04 -0800

    Create home page div and add nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
+import NavBar from "./components/NavBar";
 
 function App() {
-
   return (
-    <>
+    <div className="home-page">
+      <NavBar/>
       <h1>Welcome to the home page!</h1>
       <a href="store">Store page</a>
-    </>
+    </div>
   )
 }
 
