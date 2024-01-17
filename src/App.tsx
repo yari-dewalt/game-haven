b6ec99b6a5727a30b8e1c70b6713dd3aea85ac86
yari-dewalt <yari.dewalt@gmail.com>
2024-01-16 22:20:42 -0800

    Add navigate function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
 import NavBar from "./components/NavBar";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+
   return (
     <div className="home-page">
-      <NavBar/>
+      <NavBar navigate={() => navigate("/store")}/>
       <h1>Welcome to the home page!</h1>
       <a href="store">Store page</a>
     </div>
