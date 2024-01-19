5b91e04ffa98facc79a82371ba0690c42e2d0d6d
yari-dewalt <yari.dewalt@gmail.com>
2024-01-18 17:47:35 -0800

    Update navbar functionality

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar";
 import logo from "../assets/logo.png";
 import shoppingcart from "../assets/shopping_cart.svg";
 
-function NavBar({ navigate, onStorePage, handleStoreData })
+function NavBar({ navigate, onStorePage, handleStoreData, onSectionChange, handleSearched })
 {
   return (
     <nav className="nav-bar">
@@ -11,7 +11,7 @@ function NavBar({ navigate, onStorePage, handleStoreData })
         <img id="game-haven-logo" src={logo} alt="game haven logo"></img>
         <h2 id="game-haven-text">Game Haven</h2>
       </a>
-      <SearchBar navigate={navigate} onStorePage={onStorePage} handleStoreData={handleStoreData}/>
+      <SearchBar navigate={navigate} onStorePage={onStorePage} handleStoreData={handleStoreData} onSectionChange={onSectionChange} handleSearched={handleSearched}/>
       <button className="shopping-cart-button">
         <img id="shopping-cart-icon" src={shoppingcart} alt="shopping cart icon"></img>
       </button>
