0a36861b2782e2296a4733c9823230a17ca9ae34
yari-dewalt <yari.dewalt@gmail.com>
2024-01-18 17:48:13 -0800

    Update to take in handleSearched prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import NavBar from "./components/NavBar";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function App() {
+function App({ handleSearched }) {
   const navigate = useNavigate();
 
   return (
     <div className="home-page">
-      <NavBar navigate={() => navigate("/store")}/>
+      <NavBar navigate={() => navigate("/store")} handleSearched={handleSearched}/>
       <h1>Welcome to the home page!</h1>
       <a href="store">Store page</a>
     </div>
