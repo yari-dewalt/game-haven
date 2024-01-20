3b8c4dd4f610194eb798158d38d52962929d2864
yari-dewalt <yari.dewalt@gmail.com>
2024-01-19 20:02:02 -0800

    Create component for image navigation

---
+++ src/components/ImageNav.tsx
@@ -0,0 +1,12 @@
+function ImageNav({ images, currentIndex, changeIndex, isHovered })
+{
+  return (
+    <div className="image-nav">
+      {images.map((image, index) =>
+        <button key={index} className={`image-nav-circle ${index == currentIndex ? "selected" : ''} ${isHovered ? " shown" : ''}`} onClick={() => changeIndex(index)}></button>
+      )}
+    </div>
+  )
+}
+
+export default ImageNav;
