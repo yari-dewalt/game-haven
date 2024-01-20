2e2d78b522e9539f0770408a4033b4f1b1cb40e3
yari-dewalt <yari.dewalt@gmail.com>
2024-01-19 20:36:10 -0800

    Import styling and add onHover class for parent div

--- src/components/ImageNav.tsx
+++ src/components/ImageNav.tsx
@@ -1,7 +1,9 @@
+import "../styles/ImageNav.css";
+
 function ImageNav({ images, currentIndex, changeIndex, isHovered })
 {
   return (
-    <div className="image-nav">
+    <div className={`image-nav ${isHovered ? "shown": ''}`}>
       {images.map((image, index) =>
         <button key={index} className={`image-nav-circle ${index == currentIndex ? "selected" : ''} ${isHovered ? " shown" : ''}`} onClick={() => changeIndex(index)}></button>
       )}
