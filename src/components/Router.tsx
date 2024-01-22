41a58c5734f8922e90cd732b3adbee46b4639d7d
yari-dewalt <yari.dewalt@gmail.com>
2024-01-21 21:45:02 -0800

    Create overhead for managing cart

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import App from "../App";
 import Store from "./Store";
 import ErrorPage from "./ErrorPage";
@@ -7,20 +7,51 @@ import ErrorPage from "./ErrorPage";
 function Router()
 {
   const [searched, setSearched] = useState(false);
+  const [cartInfo, setCartInfo] = useState([]);
+  const [showCart, setShowCart] = useState(false);
+
+  useEffect(() => {
+    const storedCartInfo = localStorage.getItem("cart-info");
+    if (storedCartInfo) {
+      setCartInfo(JSON.parse(storedCartInfo));
+    }
+  }, [])
 
   function handleSearched(value) {
     setSearched(value);
   }
 
+  function handleShowCart(value) {
+    setShowCart(value);
+  }
+
+  function addToCart(game) {
+    console.log(game);
+    const updatedCartInfo = [...cartInfo, game];
+    setCartInfo(updatedCartInfo);
+    localStorage.setItem("cart-info", JSON.stringify(updatedCartInfo));
+  }
+
+  function deleteFromCart(game) {
+    const updatedCartInfo = cartInfo.filter((item) => item != game);
+    setCartInfo(updatedCartInfo);
+    localStorage.setItem("cart-info", JSON.stringify(updatedCartInfo));
+  }
+
+  function clearCart() {
+    setCartInfo([]);
+    localStorage.setItem("cart-info", JSON.stringify([]));
+  }
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <App handleSearched={handleSearched}/>,
+      element: <App handleSearched={handleSearched} cartInfo={cartInfo} showCart={showCart} handleShowCart={handleShowCart} deleteFromCart={deleteFromCart} clearCart={clearCart}/>,
       errorElement: <ErrorPage/>,
     },
     {
       path: "store",
-      element: <Store searched={searched}/>,
+      element: <Store searched={searched} cartInfo={cartInfo} showCart={showCart} handleShowCart={handleShowCart} addToCart={addToCart} deleteFromCart={deleteFromCart} clearCart={clearCart}/>,
     },
   ]);
 
