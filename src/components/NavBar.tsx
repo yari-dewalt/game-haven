239be34e70a28ce1aff5a4d28d4a549654fa8efd
yari-dewalt <yari.dewalt@gmail.com>
2024-01-21 21:46:00 -0800

    Make shopping cart button show cart

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar";
 import logo from "../assets/logo.png";
 import shoppingcart from "../assets/shopping_cart.svg";
 
-function NavBar({ navigate, onStorePage, handleStoreData, onSectionChange, handleSearched })
+function NavBar({ navigate, onStorePage, handleStoreData, onSectionChange, handleSearched, handleShowCart })
 {
   return (
     <nav className="nav-bar">
@@ -12,7 +12,7 @@ function NavBar({ navigate, onStorePage, handleStoreData, onSectionChange, handl
         <h2 id="game-haven-text">Game Haven</h2>
       </a>
       <SearchBar navigate={navigate} onStorePage={onStorePage} handleStoreData={handleStoreData} onSectionChange={onSectionChange} handleSearched={handleSearched}/>
-      <button className="shopping-cart-button">
+      <button className="shopping-cart-button" onClick={() => handleShowCart(true)}>
         <img id="shopping-cart-icon" src={shoppingcart} alt="shopping cart icon"></img>
       </button>
     </nav>
