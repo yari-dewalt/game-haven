d2545d4830cf1f2be450d0160c1bce10f50e9a78
yari-dewalt <yari.dewalt@gmail.com>
2024-01-21 21:44:26 -0800

    Create Checkout Modal screen

---
+++ src/components/CheckoutModal.tsx
@@ -0,0 +1,18 @@
+import "../styles/CheckoutModal.css";
+
+function CheckoutModal({ isOpen, totalItems, totalPrice })
+{
+  if (!isOpen) return null;
+
+  return (
+    <div className="checkout-modal-background">
+      <div className="checkout-modal">
+        <h2>Checkout Successful!</h2>
+        <div className="checkmark">✓</div>
+        <p>{`You bought ${totalItems} item(s) for $${totalPrice.toFixed(2)}`}</p>
+      </div>
+    </div>
+  )
+}
+
+export default CheckoutModal;
