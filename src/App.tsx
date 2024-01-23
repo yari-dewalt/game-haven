063b915a2f390efffe4967de2c89ffaae31dec51
yari-dewalt <yari.dewalt@gmail.com>
2024-01-22 18:18:33 -0800

    Update props for handling clicked search

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 import { useNavigate } from "react-router-dom";
 
-function App({ handleSearched, cartInfo, showCart, handleShowCart, deleteFromCart, clearCart }) {
+function App({ handleSearched, cartInfo, showCart, handleShowCart, deleteFromCart, clearCart, handleClickedSearch }) {
   const navigate = useNavigate();
 
   return (
     <div className="home-page">
-      <NavBar navigate={() => navigate("/store")} handleSearched={handleSearched} handleShowCart={handleShowCart}/>
+      <NavBar navigate={() => navigate("/store")} handleSearched={handleSearched} handleShowCart={handleShowCart} handleClickedSearch={handleClickedSearch}/>
       {showCart && <Cart cartInfo={cartInfo} handleShowCart={handleShowCart} deleteFromCart={deleteFromCart} clearCart={clearCart}/>}
       <h1>Welcome to the home page!</h1>
       <a href="store">Store page</a>
