d2271e9ffd11f3b4fb55fde303b9f6a286e66a67
yari-dewalt <yari.dewalt@gmail.com>
2024-01-23 19:02:41 -0800

    Update home page content

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,32 @@ import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 import { useNavigate } from "react-router-dom";
 import backgroundVideo from "./assets/background-video.mp4";
+import githubIcon from "./assets/github.svg";
 
 function App({ handleSearched, cartInfo, showCart, handleShowCart, deleteFromCart, clearCart, handleClickedSearch }) {
   const navigate = useNavigate();
 
   return (
+    <>
     <div className="home-page">
+      <NavBar navigate={() => navigate("/store")} handleSearched={handleSearched} handleShowCart={handleShowCart} handleClickedSearch={handleClickedSearch}/>
+      {showCart && <Cart cartInfo={cartInfo} handleShowCart={handleShowCart} deleteFromCart={deleteFromCart} clearCart={clearCart}/>}
       <div className="home-page-content">
-        <NavBar navigate={() => navigate("/store")} handleSearched={handleSearched} handleShowCart={handleShowCart} handleClickedSearch={handleClickedSearch}/>
-        {showCart && <Cart cartInfo={cartInfo} handleShowCart={handleShowCart} deleteFromCart={deleteFromCart} clearCart={clearCart}/>}
-        <h1>Welcome to the home page!</h1>
-        <a href="store">Store page</a>
+        <div className="welcome">
+          <h1>Game Haven</h1>
+          <p>An imitation of a real online game store.</p>
+          <a className="github-redir" href="https://github.com/yari-dewalt">
+              <img src={githubIcon} alt="github icon"></img>
+              <p>yari-dewalt</p>
+          </a>
+        </div>
+        <a className="to-store" href="store">To Store →</a>
       </div>
-      <video className="background-video" autoPlay loop muted>
-        <source src={backgroundVideo} type="video/mp4"></source>
-      </video>
     </div>
+    <video className="background-video" autoPlay loop muted>
+      <source src={backgroundVideo} type="video/mp4"></source>
+    </video>
+    </>
   )
 }
 
